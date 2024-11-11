fe85df1ac1c2b29ea4404dcfbf2970bb9e1fc092
Shifa Sadaat <shifasadaat@Shifas-MBP.home>
2024-11-11 16:00:15 -0700

    amended bug in category selection

--- src/main/java/com/example/sundevilslibrary/SellerPage.java
+++ src/main/java/com/example/sundevilslibrary/SellerPage.java
@@ -36,45 +36,64 @@ public class SellerPage extends HBox { // extends Parent
     public SellerPage(){
         VBox vbox = new VBox(30);
 
+        URL url = getClass().getResource("/images/library_logo.jpg");
+        Image storeLogoImage = new Image(url.toString());
+        ImageView storeLogo = new ImageView(storeLogoImage);
+        storeLogo.setFitWidth(200);
+        storeLogo.setFitHeight(200);
+        storeLogo.setPreserveRatio(true);
+        Circle circleClip = new Circle(storeLogo.getFitWidth() / 2);
+        circleClip.setCenterX(storeLogo.getFitWidth() / 2);
+        circleClip.setCenterY(storeLogo.getFitHeight() / 2);
+        storeLogo.setClip(circleClip);
 
         Label pageLabel = new Label("List your Book Here!");
         pageLabel.getStyleClass().add("title");
 
         Label titleLabel = new Label("Title");
+        titleLabel.setStyle("-fx-font-size: 12pt;");
         TextField title = new TextField();
+        title.setPromptText("Enter Book Name (e.g., Cat In The Hat)");
         title.getStyleClass().add("textfield");
 
         Label condition = new Label("Condition");
+        condition.setStyle("-fx-font-size: 12pt;");
         ComboBox<String> comboBox1 = new ComboBox<>();
-        comboBox1.getItems().addAll("New", "Used - Like New", "Used");
-        comboBox1.setValue("New");
+        comboBox1.getItems().addAll( "Used - Like New", "Good", "Moderate", "Poor");
+        comboBox1.setPromptText("Select Book Condition");
 
         Label category = new Label("Category");
+        category.setStyle("-fx-font-size: 12pt;");
         ComboBox<String> comboBox2 = new ComboBox<>();
         comboBox2.getItems().addAll("Natural Science", "Computer", "Math", "English Language", "Other");
-        comboBox2.setValue("Natural Science");
+        comboBox2.setPromptText("Select Book Category");;
         Label origPrice = new Label("Original Price");
+        origPrice.setStyle("-fx-font-size: 12pt;");
         TextField numberField = new TextField();
+        numberField.setPromptText("Enter Original Price (e.g., 29.99)");
         numberField.setTextFormatter(new TextFormatter<>(new NumberStringConverter()));
         numberField.getStyleClass().add("textfield");
 
         VBox errorV = new VBox();
 
+        System.out.printf("%s", comboBox2.getValue());
+        System.out.printf("%s", comboBox1.getValue());
         Button submitButton = new Button("Generate Price");
+        submitButton.setStyle("-fx-font-size: 12pt;");
         submitButton.getStyleClass().add("submit");
         submitButton.setPrefWidth(140);
         submitButton.setOnAction(event-> {
-            if (title.getText().trim().isEmpty() || numberField.getText().trim().isEmpty() ){
+            if (title.getText().trim().isEmpty() || numberField.getText().trim().isEmpty() || comboBox1.getValue()==null || comboBox2.getValue()==null ){
 
                 errorV.setVisible(true);
-                } else{
-            String bookTitle = title.getText();
-            Double startingPrice = Double.parseDouble(numberField.getText());
-            String cond = comboBox1.getValue();
-            String cat = comboBox2.getValue();
-            errorV.setVisible(false);
-            generateListing(vbox, bookTitle, startingPrice, cond, cat);
-              }
+            } else{
+                String bookTitle = title.getText();
+                Double startingPrice = Double.parseDouble(numberField.getText());
+                String cond = comboBox1.getValue();
+                String cat = comboBox2.getValue();
+                errorV.setVisible(false);
+                generateListing(vbox, bookTitle, startingPrice, cond, cat);
+            }
         });
 
         // require numberField to only accept numbers
@@ -91,21 +110,24 @@ public class SellerPage extends HBox { // extends Parent
             });
         });
         Button logOff = new Button("Log Out");
+        logOff.setStyle("-fx-font-size: 12pt;");
         logOff.getStyleClass().add("submit");
         logOff.setOnAction(event -> {
             loggedOut.set(1);
         });
         VBox logOutBox = new VBox(30);
-        logOutBox.getChildren().add(logOff);
-        logOutBox.setPadding(new Insets(20));
+        logOutBox.getChildren().addAll(logOff, storeLogo);
+        logOutBox.setPadding(new Insets(50));
         logOutBox.setAlignment(Pos.BASELINE_LEFT);
-        logOutBox.setPrefWidth(40);
+        //logOutBox.setPrefWidth(40);
+
 
         errorV.setPrefHeight(40);
         errorV.setMaxWidth(350);
         errorV.getStyleClass().add("errorbox");
 
         Label errorMsg = new Label("Fill in all fields before generating Price!");
+        errorMsg.setStyle("-fx-font-size: 12pt;");
         errorMsg.getStyleClass().add("error");
         Font font3 = Font.font("Helvetica", FontWeight.BOLD, FontPosture.REGULAR, 16);
         errorMsg.setFont(font3);
@@ -140,22 +162,31 @@ public class SellerPage extends HBox { // extends Parent
         Label confirmPurch = new Label("Confirm Your Listing");
         confirmPurch.getStyleClass().add("title");
         Label title1 = new Label("Title: \t" + title);
+        title1.setStyle("-fx-font-size: 12pt;");
         DecimalFormat formatPrices = new DecimalFormat("#.##");
-        if (condition.equals("New")){
+        if (condition.equals("Used - Like New")){
             price *= 0.85;
-        } else if (condition.equals("Used")){
-            price *= 0.6;
-        } else {
+        } else if (condition.equals("Good")){
             price *= 0.7;
+        } else if (condition.equals("Moderate")) {
+            price *= 0.6;
+        }else {
+            price *= 0.5;
         }
+
         String prices = formatPrices.format(price);
         Label newPrice = new Label("Calculated Price: \t" + prices);
+        newPrice.setStyle("-fx-font-size: 12pt;");
         Label Condition = new Label("Condition: \t" + condition);
+        Condition.setStyle("-fx-font-size: 12pt;");
         Label Category = new Label("Category: \t" + category);
+        Category.setStyle("-fx-font-size: 12pt;");
 
         Button confirmListing = new Button("Confirm Listing");
+        confirmListing.setStyle("-fx-font-size: 12pt;");
         confirmListing.getStyleClass().add("submit");
         Button cancelListing = new Button("Cancel Listing");
+        cancelListing.setStyle("-fx-font-size: 12pt;");
         cancelListing.getStyleClass().add("submit");
 
         cancelListing.setOnAction(event ->{
@@ -175,10 +206,11 @@ public class SellerPage extends HBox { // extends Parent
                 System.out.println("Failed to open file Books.txt");
             }
             this.getChildren().remove(vbox2);
-            vbox.getChildren().add(new Label("Your Book was successfully listed!"));
+            Label successLabel = new Label("Your Book was successfully listed!");
+            successLabel.setStyle("-fx-font-size: 18pt; -fx-text-fill: red;");
+
+            vbox.getChildren().add(successLabel);
             this.getChildren().add(vbox);
-
-
             this.setAlignment(Pos.CENTER);
         });
 
@@ -188,12 +220,8 @@ public class SellerPage extends HBox { // extends Parent
         receipts.setPadding(new Insets(30));
         receipts.setAlignment(Pos.CENTER_LEFT);
         vbox2.getChildren().addAll(confirmPurch, receipts, confirmListing, cancelListing );
-
-
-
         vbox2.setAlignment(Pos.TOP_CENTER);
         vbox2.setPadding(new Insets(30));
-
 
         this.getChildren().add(vbox2);
         this.setAlignment(Pos.CENTER);
@@ -202,6 +230,4 @@ public class SellerPage extends HBox { // extends Parent
     public ObservableValue<? extends Number> getLoggedOut() {
         return loggedOut;
     }
-
-
 }
